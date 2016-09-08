a629cb3053d9c81a69ed9abb24e9c977b0fc743a
IWAMOTO Kouichi <sue@iwmt.org>
2016-09-08 10:50:08 +0900

    アンインストーラ更新後のテストバージョン

--- teraterm/tools/chocolateyinstall.ps1
+++ teraterm/tools/chocolateyinstall.ps1
@@ -2,7 +2,8 @@
 
 $packageName= 'teraterm'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://osdn.jp/dl/ttssh2/teraterm-4.92.exe'
+#$url        = 'https://osdn.jp/dl/ttssh2/teraterm-4.92.exe'
+$url        = 'http://doda.teraterm.net/test/teraterm-20160908_101950.exe'
 $url64      = ''
 
 $packageArgs = @{
@@ -14,7 +15,8 @@ $packageArgs = @{
 
   softwareName  = 'Tera Term *'
 
-  checksum      = '4B42863FEFA83DAE0140F531939D334061F210F4092FA02B2E02063ABDC94EC0'
+#  checksum      = '4B42863FEFA83DAE0140F531939D334061F210F4092FA02B2E02063ABDC94EC0'
+  checksum      = 'FA07D1778DA6D5088E2EF1C8DE37EAE63BFFADA913BE6ECA38175E8D4E8A79C0'
   checksumType  = 'sha256'
   checksum64    = ''
   checksumType64= 'sha256'
