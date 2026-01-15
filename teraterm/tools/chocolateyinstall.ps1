f72b1c5f2b0ec3f7b73857045c4a4c7a5a257f47
nmaya <5668936+nmaya@users.noreply.github.com>
2026-01-15 19:20:28 +0900

    Tera Term 5.5.1

--- teraterm/tools/chocolateyinstall.ps1
+++ teraterm/tools/chocolateyinstall.ps1
@@ -2,7 +2,19 @@
 
 $packageName= 'teraterm'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/TeraTermProject/teraterm/releases/download/v4.108/teraterm-4.108.exe'
+
+if ($env:PROCESSOR_ARCHITEW6432) { $osArch = $env:PROCESSOR_ARCHITEW6432 }
+
+$url         = 'https://github.com/TeraTermProject/teraterm/releases/download/v5.5.1/teraterm-5.5.1-x86.exe'
+$urlchecksum = '992150df0899ae5d3517523f485aee15baedc45fc0388101b8608623a098971d'
+
+if ($osArch -eq 'ARM64') {
+    $url      = 'https://github.com/TeraTermProject/teraterm/releases/download/v5.5.1/teraterm-5.5.1-arm64.exe'
+    $checksum = 'a558576648d4dbda6f1be24f676943df6e7fbbe2ce214ecc5f6f04889843f4eb'
+} elseif ($osArch -eq 'AMD64') {
+    $url      = 'https://github.com/TeraTermProject/teraterm/releases/download/v5.5.1/teraterm-5.5.1-x64.exe'
+    $checksum = 'fe1f8170e22f4bb76c1a13c04e8c394d64b9c633683df2d006ace39ca7f97f57'
+}
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,9 +22,9 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
 
-  softwareName  = 'Tera Term *'
+  softwareName  = 'Tera Term 5.*'
 
-  checksum      = '3db07544a71cd140d75234a7883945da4878d077ba03099e1362bb2d4783e50a'
+  checksum      = $checksum
   checksumType  = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
