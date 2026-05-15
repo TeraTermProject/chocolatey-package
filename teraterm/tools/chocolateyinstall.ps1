fe5f1007bf5b7aa2308366480c6f30750cd153f0
nmaya <5668936+nmaya@users.noreply.github.com>
2026-05-15 22:00:16 +0900

    Tera Term 5.6.1

--- teraterm/tools/chocolateyinstall.ps1
+++ teraterm/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageName= 'teraterm'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url      = 'https://github.com/TeraTermProject/teraterm/releases/download/v5.6.0/teraterm-5.6.0-x86.exe'
-$checksum = '88986d8cb16cc0b7b3776872daaceefa0ffdc6cbe5fa0b0838a6a4c85fd981fe'
+$url      = 'https://github.com/TeraTermProject/teraterm/releases/download/v5.6.1/teraterm-5.6.1-x86.exe'
+$checksum = 'f39d3cb6f6dde7808d5760dbf0fd02a077b5bea4d291f1b5c2b7b36d9bac3b74'
 
 # x64
-$url64      = 'https://github.com/TeraTermProject/teraterm/releases/download/v5.6.0/teraterm-5.6.0-x64.exe'
-$checksum64 = '05afdcf1e26aa24f0392a1e60e15626f126ff3bf416565e453857889c678e534'
+$url64      = 'https://github.com/TeraTermProject/teraterm/releases/download/v5.6.1/teraterm-5.6.1-x64.exe'
+$checksum64 = 'e2035d4dc05757aae5913c6542b973028b17a311fe03e8561d138b5255792dbc'
 
 # ARM64
 #   https://docs.chocolatey.org/en-us/create/functions/get-osarchitecturewidth/#notes
